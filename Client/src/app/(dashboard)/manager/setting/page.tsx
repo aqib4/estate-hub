ef46dfa662c6a54d8f56a28a9c8435e33645e2e1
app <app@192.168.1.2>
2026-01-26 22:59:54 +0500

    manager setting forms updated

--- Client/src/app/(dashboard)/manager/setting/page.tsx
+++ Client/src/app/(dashboard)/manager/setting/page.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import SettingsForm from '@/components/SettingsForm/SettingsForm'
+import { useGetAuthUserQuery, useUpdateManagerSettingsMutation } from '@/state/api'
+import { userInfo } from 'os';
+import React, { use } from 'react'
+
+type initialDataType = {
+    name: string;
+    email: string;
+    phoneNumber: string;
+}
 
 function Settings() {
+    
+     const [updateManagerSettings]=useUpdateManagerSettingsMutation();
+     const {data:authUser,error}=useGetAuthUserQuery();
+     console.log('Auth User Data:', authUser);
+
+     const initialData:initialDataType =  {
+        name: authUser?.userInfo?.name || '',
+        email: authUser?.userInfo?.email || '',
+        phoneNumber: authUser?.userInfo?.phoneNumber || '',
+     }
+
+     const handleSubmit = async (data: initialDataType) =>{
+           
+     }
+
+
+
+     
+
+
   return (
-    <div>Settings</div>
+    <SettingsForm  
+    
+    />
+
   )
 }
 
