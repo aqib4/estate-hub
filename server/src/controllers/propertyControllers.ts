c6c606b4b6e9ac802662c43c195bfe25e0cdaeff
app <app@192.168.1.2>
2026-02-07 22:48:45 +0500

    fixed issues

--- server/src/controllers/propertyControllers.ts
+++ server/src/controllers/propertyControllers.ts
@@ -22,7 +22,7 @@ export const getAllProperties = async (req: Request, res: Response): Promise<voi
             latitude,
             longitude,
           }= req.query;
-
+         
           let whereConditions: Prisma.Sql[] = [];
 
           if(favoriteIds){
