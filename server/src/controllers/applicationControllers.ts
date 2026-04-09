30c89163e6979c18e396784c179b9659d3d8a4c4
app <app@192.168.1.3>
2026-04-09 11:08:28 +0500

    applicatio api create sapplicationa  lease

--- server/src/controllers/applicationControllers.ts
+++ server/src/controllers/applicationControllers.ts
@@ -1,61 +1,171 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+import { connect } from "http2";
 
-const prisma= new PrismaClient();
+const prisma = new PrismaClient();
 
- const listApplications = async(req:Request,res:Response):Promise<void> =>{
-       try {
-             const {userID,userType}= req.query;
-             let whereClause= {};
+const listApplications = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { userID, userType } = req.query;
+    let whereClause = {};
 
-             if(userID && userType){
-                if(userType==="tenant"){
-                    whereClause = {
-                        tenantCognitoId: String(userID)
-                    };
-                }
-                else if(userType==="manager"){
-                    whereClause ={
-                        property : {
-                            mamagerCognitoId:String(userID)
-                        }
-                    }
-                }
+    if (userID && userType) {
+      if (userType === "tenant") {
+        whereClause = {
+          tenantCognitoId: String(userID),
+        };
+      } else if (userType === "manager") {
+        whereClause = {
+          property: {
+            managerCognitoId: String(userID),
+          },
+        };
+      }
+    }
+
+    const applications = await prisma.application.findMany({
+      where: whereClause,
+      include: {
+        property: {
+          include: {
+            location: true,
+            manager: true,
+          },
+        },
+      },
+    });
+
+    function calculateNextPayments(startDate: Date): Date {
+      const Today = new Date();
+      const nextPaymentDate = new Date(startDate);
+
+      while (nextPaymentDate <= Today) {
+        nextPaymentDate.setMonth(nextPaymentDate.getMonth() + 1);
+      }
+      return nextPaymentDate;
+    }
+
+    const formatApplications = await Promise.all(
+      applications.map(async (app) => {
+        const lease = await prisma.lease.findFirst({
+          where: {
+            tenant: {
+              cognitoId: app.tenantCognitoId,
+            },
+            propertyId: app.propertyId,
+            },
+            orderBy: {
+             startDate: "desc",
+            },
+        });
+
+        return {
+          ...app,
+          property: {
+            ...app.property,
+            address: app.property.location.address,
+          },
+          manager: app.property.manager,
+          lease: lease
+            ? {
+                ...lease,
+                nextPaymentDate: calculateNextPayments(lease.startDate),
               }
+            : null,
+        };
+      })
+    );
 
-             const applications= await prisma.application.findMany({
-                where: whereClause,
-                include:{
-                    property: {
-                        include:{
-                            location: true,
-                            manager: true
-                        }
+    res.json(formatApplications);
+  } catch (err: any) {
+    res
+      .status(500)
+      .json({ message: `Error Retrieving applications: ${err.message}` });
+  }
+};
+
+const createApplication = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { 
+            applicationDate,
+            status,
+            tenantCognitoId,
+            propertyId,
+            name,
+            email,
+            phoneNumber,
+            message   
+        } = req.body;
+
+        const property=await prisma.property.findUnique({
+            where:{id:propertyId},
+            select:{pricePerMonth:true,securityDeposit:true}
+        })
+
+        if(!property){
+            res.status(404).json({message:"Property not found"});
+            return;
+        }
+        
+        const newApplication= await prisma.$transaction(async (prisma) => {
+            //create lease first
+            const least = await prisma.lease.create({
+                data: {
+                    startDate: new Date(),
+                    endDate: new Date(
+                        new Date().setFullYear(new Date().getFullYear() + 1)
+                    ),
+                    rent: property.pricePerMonth,
+                    deposit: property.securityDeposit,
+                    property:{
+                        connect: { id: propertyId }
+                    },
+                    tenant:{
+                        connect: { cognitoId: tenantCognitoId }
                     }
                 }
-             })
-
-             function calculateNextPayments(startDate:Date):Date{
-                  const Today=new Date();
-                  const nextPaymentDate= new Date(startDate);
-
-                  while(nextPaymentDate <= Today){
-                    nextPaymentDate.setMonth(nextPaymentDate.getMonth()+1);
-                  }
-                    return nextPaymentDate;
-             }
+            });
              
-             const formatApplications= await Promise.all(
-                app
-             )
+            //then create application
+            const application = await prisma.application.create({
+                data: {
+                    applicationDate: new Date(applicationDate),
+                    status,
+                    tenantCognitoId,
+                    propertyId,
+                    name,
+                    email,
+                    phoneNumber,
+                    message,
+                    property : {
+                    connect :{ id: propertyId} 
+                    },
+                    tenant: {
+                        connect: { cognitoId: tenantCognitoId }
+                    },
+                    lease: {
+                        connect: { id: least.id }
+                    }
+                },
+                include: {
+                    property:true,
+                    tenant:true,
+                    lease:true
+                }
+            });
+            return application;
 
+     });
 
-             
-       } catch (err:any) {
-        res.status(500).json({message:`Error Retrieving Lease: ${err.message}`});
-       }
-}
+        res.status(201).json({
+            message:"Application created successfully",
+            application:newApplication
+        })
 
-
-
-export  {  };
+    } catch (err: any) {
+        res
+        .status(500)
+        .json({ message: `Error Creating application: ${err.message}` });
+    }
+    }
+export { listApplications };
